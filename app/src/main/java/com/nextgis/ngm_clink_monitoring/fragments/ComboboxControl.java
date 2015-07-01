ce027d6f54d577986e94fcf01424b708d3f09aef
NikitaFeodonit <nikitafeodonit@yandex.com>
2015-07-01 20:33:46 +0300

    Design of the ComboboxControl (#100, #104)

--- app/src/main/java/com/nextgis/ngm_clink_monitoring/fragments/ComboboxControl.java
+++ app/src/main/java/com/nextgis/ngm_clink_monitoring/fragments/ComboboxControl.java
@@ -24,16 +24,38 @@ package com.nextgis.ngm_clink_monitoring.fragments;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+import android.widget.TextView;
 import com.nextgis.ngm_clink_monitoring.map.FoclDictItem;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 
 
+/**
+ * A modified Spinner that doesn't automatically select the first entry in the list.
+ * <p/>
+ * Shows the prompt if nothing is selected.
+ * <p/>
+ * Limitations: does not display prompt if the entry list is empty.
+ * <p/>
+ * see http://stackoverflow.com/a/3427058/4727406
+ * <p/>
+ * Warning! This has been tested on Android 1.5 through 4.2, but buyer beware! Because this solution relies
+ * on reflection to call the private AdapterView.setNextSelectedPositionInt() and
+ * AdapterView.setSelectedPositionInt(), it's not guaranteed to work in future OS updates. It seems
+ * likely that it will, but it is by no means guaranteed.
+ */
 public class ComboboxControl
         extends Spinner
 
@@ -68,52 +90,139 @@ public class ComboboxControl
     }
 
 
+    @Override
+    public void setAdapter(SpinnerAdapter origSpinnerAdapter)
+    {
+        final SpinnerAdapter adapter = (SpinnerAdapter) java.lang.reflect.Proxy.newProxyInstance(
+                origSpinnerAdapter.getClass().getClassLoader(), new Class[] {SpinnerAdapter.class},
+                new SpinnerAdapterProxy(origSpinnerAdapter));
+
+        super.setAdapter(adapter);
+
+        try {
+            final Method m =
+                    AdapterView.class.getDeclaredMethod("setNextSelectedPositionInt", int.class);
+            m.setAccessible(true);
+            m.invoke(this, -1);
+
+            final Method n =
+                    AdapterView.class.getDeclaredMethod("setSelectedPositionInt", int.class);
+            n.setAccessible(true);
+            n.invoke(this, -1);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    /**
+     * Intercepts getView() to display the prompt if position < 0
+     */
+    protected class SpinnerAdapterProxy
+            implements InvocationHandler
+    {
+        protected SpinnerAdapter mSpinnerAdapter;
+        protected Method         getView;
+
+
+        protected SpinnerAdapterProxy(SpinnerAdapter spinnerAdapter)
+        {
+            mSpinnerAdapter = spinnerAdapter;
+            try {
+                this.getView = SpinnerAdapter.class.getMethod(
+                        "getView", int.class, View.class, ViewGroup.class);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+
+        public Object invoke(
+                Object proxy,
+                Method m,
+                Object[] args)
+                throws Throwable
+        {
+            try {
+                return m.equals(getView) && (Integer) (args[0]) < 0
+                       ? getView(
+                        (Integer) args[0], (View) args[1], (ViewGroup) args[2])
+                       : m.invoke(mSpinnerAdapter, args);
+            } catch (InvocationTargetException e) {
+                throw e.getTargetException();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+
+        protected View getView(
+                int position,
+                View convertView,
+                ViewGroup parent)
+                throws IllegalAccessException
+        {
+            if (position < 0) {
+                final TextView v = (TextView) ((LayoutInflater) getContext().getSystemService(
+                        Context.LAYOUT_INFLATER_SERVICE)).inflate(
+                        android.R.layout.simple_spinner_item, parent, false);
+                v.setText(getPrompt());
+                return v;
+            }
+            return mSpinnerAdapter.getView(position, convertView, parent);
+        }
+    }
+
+
     public void setValues(FoclDictItem dictItem)
     {
         mAliasValueMap = new HashMap<>();
 
         ArrayAdapter<String> spinnerArrayAdapter =
                 new ArrayAdapter<>(mContext, android.R.layout.simple_spinner_item);
-        setAdapter(spinnerArrayAdapter);
 
-        for (Map.Entry<String, String> entry : dictItem.entrySet()) {
-            String value = entry.getKey();
-            String value_alias = entry.getValue();
+        if (null != dictItem) {
+            for (Map.Entry<String, String> entry : dictItem.entrySet()) {
+                String value = entry.getKey();
+                String value_alias = entry.getValue();
 
-            mAliasValueMap.put(value_alias, value);
-            spinnerArrayAdapter.add(value_alias);
+                mAliasValueMap.put(value_alias, value);
+                spinnerArrayAdapter.add(value_alias);
+            }
+
+            spinnerArrayAdapter.sort(
+                    new Comparator<String>()
+                    {
+                        @Override
+                        public int compare(
+                                String lhs,
+                                String rhs)
+                        {
+                            if (null == lhs && null == rhs) {
+                                return 0;
+                            }
+
+                            if (null == lhs) {
+                                return -1;
+                            }
+
+                            if (null == rhs) {
+                                return 1;
+                            }
+
+                            return lhs.compareToIgnoreCase(rhs);
+                        }
+                    });
+
+        } else {
+            setEnabled(false);
         }
 
-        spinnerArrayAdapter.sort(
-                new Comparator<String>()
-                {
-                    @Override
-                    public int compare(
-                            String lhs,
-                            String rhs)
-                    {
-                        if (null == lhs && null == rhs) {
-                            return 0;
-                        }
-
-                        if (null == lhs) {
-                            return -1;
-                        }
-
-                        if (null == rhs) {
-                            return 1;
-                        }
-
-                        return lhs.compareToIgnoreCase(rhs);
-                    }
-                });
 
         // The drop down view
         spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        float minHeight = TypedValue.applyDimension(
-                TypedValue.COMPLEX_UNIT_DIP, 14, getResources().getDisplayMetrics());
-        setPadding(0, (int) minHeight, 0, (int) minHeight);
+        setAdapter(spinnerArrayAdapter);
     }
 
 
